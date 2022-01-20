92b405f4e28380bff8240577ac2e7802f307201c
oz1114 <82308636+oz1114@users.noreply.github.com>
2022-01-20 16:38:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -697,5 +697,5 @@ if __name__ == "__main__":
     make_static_tmp_dir()
 
     #app.run(debug=options.debug, port=options.port)
-	port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
