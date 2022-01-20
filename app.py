8412f8471bba4b13ff3c6a5cbdd40c12c44efe97
oz1114 <82308636+oz1114@users.noreply.github.com>
2022-01-20 16:11:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -57,5 +57,16 @@ def handle_text_message(event):
                 TextSendMessage(text="Bot can't use profile API without user ID"))
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+
+    # create tmp dir for download content
+    make_static_tmp_dir()
+
+    app.run(debug=options.debug, port=options.port)
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(host='0.0.0.0', port=port)
