787ab5a895103d2bcae4272ece6edad8b26128ec
oz1114 <82308636+oz1114@users.noreply.github.com>
2022-01-26 18:34:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -395,128 +395,42 @@ class groupGame:
         elif text == 'test':
             bubble_string = """
         {
-          "type": "bubble",
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "image",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip3.jpg",
-                "position": "relative",
-                "size": "full",
-                "aspectMode": "cover",
-                "aspectRatio": "1:1",
-                "gravity": "center"
-              },
-              {
-                "type": "box",
-                "layout": "horizontal",
-                "contents": [
-                  {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "Brown Hotel",
-                        "weight": "bold",
-                        "size": "xl",
-                        "color": "#ffffff"
-                      },
-                      {
-                        "type": "box",
-                        "layout": "baseline",
-                        "margin": "md",
-                        "contents": [
-                          {
-                            "type": "icon",
-                            "size": "sm",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                          },
-                          {
-                            "type": "icon",
-                            "size": "sm",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                          },
-                          {
-                            "type": "icon",
-                            "size": "sm",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                          },
-                          {
-                            "type": "icon",
-                            "size": "sm",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                          },
-                          {
-                            "type": "icon",
-                            "size": "sm",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                          },
-                          {
-                            "type": "text",
-                            "text": "4.0",
-                            "size": "sm",
-                            "color": "#d6d6d6",
-                            "margin": "md",
-                            "flex": 0
-                          }
-                        ]
-                      }
-                    ]
-                  },
-                  {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "¥62,000",
-                        "color": "#a9a9a9",
-                        "decoration": "line-through",
-                        "align": "end"
-                      },
-                      {
-                        "type": "text",
-                        "text": "¥42,000",
-                        "color": "#ebebeb",
-                        "size": "xl",
-                        "align": "end"
-                      }
-                    ]
-                  }
-                ],
-                "position": "absolute",
-                "offsetBottom": "0px",
-                "offsetStart": "0px",
-                "offsetEnd": "0px",
-                "backgroundColor": "#00000099",
-                "paddingAll": "20px"
-              },
-              {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "SALE",
-                    "color": "#ffffff"
-                  }
-                ],
-                "position": "absolute",
-                "backgroundColor": "#ff2600",
-                "cornerRadius": "20px",
-                "paddingAll": "5px",
-                "offsetTop": "10px",
-                "offsetEnd": "10px",
-                "paddingStart": "10px",
-                "paddingEnd": "10px"
-              }
-            ],
-            "paddingAll": "0px"
-          }
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "md",
+    "contents": [
+      {
+        "type": "button",
+        "style": "primary",
+        "action": {
+          "type": "uri",
+          "label": "Primary style button",
+          "uri": "https://example.com"
         }
+      },
+      {
+        "type": "button",
+        "style": "secondary",
+        "action": {
+          "type": "uri",
+          "label": "Secondary style button",
+          "uri": "https://example.com"
+        }
+      },
+      {
+        "type": "button",
+        "style": "link",
+        "action": {
+          "type": "uri",
+          "label": "Link style button",
+          "uri": "https://example.com"
+        }
+      }
+    ]
+  }
+}
         """
             message = FlexSendMessage(alt_text="hello", contents=json.loads(bubble_string))
             line_bot_api.push_message(self.groupId, message)
