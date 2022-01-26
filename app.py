c754f9d7511e225ff38ba68ec885cfc1b998eb1a
oz1114 <82308636+oz1114@users.noreply.github.com>
2022-01-26 14:35:18 +0900

    LiarGame added
    거짓말쟁이게임 추가

--- app.py
+++ app.py
@@ -73,9 +73,12 @@ f.close()
 f = open('4WS.txt','r',encoding='euc-kr')
 ws4arr = f.readlines()
 f.close()
-#file_list = os.listdir('thingsQuizImage')
 f = open('thingsQuiz.txt','rt',encoding='UTF8')
 thingsq = f.readlines()
+f.close()
+f = open('LiarGame.txt','rt',encoding='UTF8')
+liarGameList = f.readlines()
+f.close()
 class groupGame:
     def __init__(self,group_id):
         self.state= 0#게임 진행 상태
@@ -87,6 +90,9 @@ class groupGame:
         self.fileTemp = []#파일 저장용 readlines
         self.timerA = Timer1(group_id,'',0)#문제 타이머
         self.roundCounter = 1#endless 랜덤게임 라운드 저장용
+        self.voted = set()#투표여부 확인용
+        self.votedCount = []#득표수
+        self.liarMan = ''
     def resetGame(self):#게임 진행 상태 리셋
         self.state = 0
         self.memberList = []
@@ -95,6 +101,9 @@ class groupGame:
         self.nowAnswer = []
         self.fileTemp = []
         self.timerA = Timer1(self.groupId,'',0)
+        self.voted = set()
+        self.votedCount = []
+        self.liarMan = ''
     def missionSuccess(self):
         line_bot_api.push_message(
             self.groupId, TextSendMessage(text='정답!!\n미션 성공!!\n게임시작을 입력해주세요'))
@@ -123,7 +132,7 @@ class groupGame:
         line_bot_api.push_message(self.groupId, TextSendMessage(text=q))
         self.timerA = Timer1(self.groupId,ans,6)
         self.timerA.start()
-    def capitalQuiz(self):
+    def capitalQuiz(self):#수도퀴즈
         global capitals
         self.nowAnswer = []
         t = randint(0,len(capitals)-1)
@@ -142,7 +151,7 @@ class groupGame:
         line_bot_api.push_message(self.groupId, TextSendMessage(text=q))
         self.timerA = Timer1(self.groupId,ans,7)
         self.timerA.start()
-    def thingsQuiz(self):
+    def thingsQuiz(self):#사물퀴즈
         global thingsq
         self.nowAnswer = []
         t = randint(0,len(thingsq)-1)
@@ -161,7 +170,26 @@ class groupGame:
         line_bot_api.push_message(self.groupId, ImageSendMessage(qimage,qimage))
         self.timerA = Timer1(self.groupId,ans,6)
         self.timerA.start()
-
+    def LiarGame(self):#거짓말쟁이 게임
+        global liarGameList
+        self.nowAnswer = []
+        t = randint(0,len(liarGameList)-1)
+        temp = liarGameList[t].split()
+        liarGameCategory = temp[0]
+        t = randint(1,len(temp)-1)
+        self.nowAnswer = temp[t:t+1]
+        liar = randint(0,len(self.memberList)-1)
+        talkOrder = ''
+        for i in range(len(self.memberList)):
+            talkOrder += self.memberList[i].display_name + ' '
+            if i==liar:
+                line_bot_api.push_message(self.memberList[i].user_id,TextSendMessage(text = '당신은 거짓말쟁이 입니다.'))
+                self.liarMan = self.memberList[i].user_id
+            else:
+                line_bot_api.push_message(self.memberList[i].user_id,TextSendMessage(text = self.nowAnswer[0]))
+        line_bot_api.push_message(self.groupId, TextSendMessage(text= '카테고리는 '+liarGameCategory
+        +'\n순서대로 정답에 관해 설명해주시면 됩니다.\n순서는 '+talkOrder))
+        
     def messageR(self,text,userId):
         if text == '게임준비' and self.state==0:
             self.state = 1
@@ -250,8 +278,47 @@ class groupGame:
                 self.capitalQuiz()
             elif r==3:
                 self.thingsQuiz()
+        elif text=='5' and self.state==2:#거짓말쟁이 게임
+            self.state = 6
+            shuffle(self.memberList)
+            self.nowMem = 0
+            self.LiarGame()
+        elif self.state==6 and userId==self.memberList[self.nowMem].user_id:#거짓말쟁이 게임 대화,투표시작
+            self.nowMem+=1
+            if self.nowMem>=len(self.memberList):
+                self.state=98
+                self.voted = set()
+                self.votedCount = [0 for i in range(len(self.memberList))]
+                line_bot_api.push_message(self.groupId, TextSendMessage(text='투표를 시작합니다\n득표수가 같은경우 앞사람으로 정해집니다.'))
+        elif self.state==98:#거짓말쟁이게임 투표
+            i = 0
+            correct = False
+            for i in range(len(self.memberList)):
+                if self.memberList[i].display_name==text:
+                    correct = True
+                    break
+            if correct and userId not in self.voted:
+                self.voted.add(userId)
+                self.votedCount[i] +=1
+            if len(self.voted) == len(self.memberList):
+                i = self.votedCount.index(max(self.votedCount))
+                if self.memberList[i].user_id==self.liarMan:
+                    self.state=97
+                    line_bot_api.push_message(self.groupId, TextSendMessage(text='거짓말쟁이를 맞추셨습니다.\n거짓말쟁이는'
+                    +self.memberList[i].display_name+'\n거짓말쟁이는 정답을 말해주세요'))
+                else:
+                    line_bot_api.push_message(self.groupId, TextSendMessage(text=self.memberList[i].display_name+'는 거짓말쟁이가 아니었습니다\n'
+                    +'거짓말쟁이는 '+line_bot_api.get_profile(self.liarMan)).display_name)
+                    self.state=1
+        elif self.state==97 and userId==self.liarMan:
+            if text==self.nowAnswer[0]:
+                line_bot_api.push_message(self.groupId, TextSendMessage(text='거짓말쟁이가 정답을 맞추었습니다.\n거짓말쟁이의 승리!!'))
+                self.state = 1
+            else:
+                line_bot_api.push_message(self.groupId, TextSendMessage(text='거짓말쟁이가 정답을 틀렸습니다.\n팀의 승리!!'))
+                self.state=1
         elif text=='reset':
-            line_bot_api.push_message(self.groupId, TextSendMessage(text='게임 설정을 Reset 합니다'))
+            #line_bot_api.push_message(self.groupId, TextSendMessage(text='게임 설정을 Reset 합니다'))
             self.resetGame()
 # function for create tmp dir for download content
 def make_static_tmp_dir():
@@ -334,16 +401,6 @@ def handle_text_message(event):
     else:
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage(text="그룹을 만들어 주세요"))
-    #elif text == '정답':
-        #timerA.flag.set()
-        #line_bot_api.reply_message(
-            #event.reply_token, TextSendMessage(text="pass"))
-    #elif text == 'time':
-        #line_bot_api.reply_message(
-            #event.reply_token, TextSendMessage(text="timer Start"))
-        #timerA = Timer1()
-        #timerA.start()
-
 
 @handler.add(JoinEvent)
 def handle_join(event):
@@ -359,6 +416,7 @@ def handle_leave():
 
 @handler.add(PostbackEvent)
 def handle_postback(event):
+    global groupsList
     if event.postback.data == 'ping':
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage(text='pong'))
@@ -377,21 +435,6 @@ def handle_beacon(event):
         TextSendMessage(
             text='Got beacon event. hwid={}, device_message(hex string)={}'.format(
                 event.beacon.hwid, event.beacon.dm)))
-
-
-@handler.add(MemberJoinedEvent)
-def handle_member_joined(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(
-            text='Got memberJoined event. event={}'.format(
-                event)))
-
-
-@handler.add(MemberLeftEvent)
-def handle_member_left(event):
-    app.logger.info("Got memberLeft event")
-
 
 @app.route('/static/<path:path>')
 def send_static_content(path):
